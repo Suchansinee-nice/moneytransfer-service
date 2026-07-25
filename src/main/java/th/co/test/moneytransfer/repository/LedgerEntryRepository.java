73b08e7892b4464d63f7c0c4c220e153bf7590ec
Suchansinee Lipikaporn <schsn.nice@gmail.com>
2026-07-25 22:00:40 +0700

    commit update createAccount service

---
+++ src/main/java/th/co/test/moneytransfer/repository/LedgerEntryRepository.java
@@ -0,0 +1,9 @@
+package th.co.test.moneytransfer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import th.co.test.moneytransfer.entity.LedgerEntry;
+
+public interface LedgerEntryRepository extends JpaRepository<LedgerEntry, Long>{
+
+}
--- src/main/java/th/co/test/moneytransfer/service/MoneyTransferService.java
+++ src/main/java/th/co/test/moneytransfer/service/MoneyTransferService.java
@@ -1,11 +1,15 @@
 package th.co.test.moneytransfer.service;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import th.co.test.moneytransfer.entity.Account;
+import th.co.test.moneytransfer.entity.LedgerEntry;
 import th.co.test.moneytransfer.repository.AccountRepository;
+import th.co.test.moneytransfer.repository.LedgerEntryRepository;
 import th.co.test.moneytransfer.request.AccountRequest;
 import th.co.test.moneytransfer.response.AccountResponse;
 
@@ -14,6 +18,7 @@ import th.co.test.moneytransfer.response.AccountResponse;
 public class MoneyTransferService {
 
     private final AccountRepository accountRepository;
+    private final LedgerEntryRepository ledgerEntryRepository;
 
     @Transactional
     public AccountResponse createAccount(AccountRequest request) {
@@ -25,8 +30,22 @@ public class MoneyTransferService {
         account.setOwnerName(request.getOwnerName());
         account.setCurrency(request.getCurrency());
         account.setBalance(request.getInitialBalance());
-
+        
+        //add account
         Account saved = accountRepository.save(account);
+        
+        //save to ledger
+        if (saved.getBalance().compareTo(BigDecimal.ZERO) > 0) {
+            LedgerEntry ledger = new LedgerEntry();
+            ledger.setAccountId(saved.getId());
+            ledger.setAmount(saved.getBalance());
+            ledger.setBalanceAfter(saved.getBalance());
+            ledger.setEntryType("CREDIT");
+            ledger.setTransferId(null);
+            
+            ledgerEntryRepository.save(ledger);
+        }
+  
 
         return AccountResponse.builder()
                 .id(saved.getId())
